bb33688df8d4521a95fcb3e7f76e79390a929b37
Zhaiyk <Zhaiykelubay@gmail.com>
2026-06-01 20:06:57 +0500

    0.1 getting started

---
+++ src/test/java/com/github/VeinyM/neobank/NeobankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.VeinyM.neobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeobankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
